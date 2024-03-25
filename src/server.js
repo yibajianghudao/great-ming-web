542d3dfd5ed9f301c9a38b5ecbd5a0fde124e530
yibajianghudao <w2027075535@outlook.com>
2024-03-25 18:43:24 +0800

    try repair sql connection

--- src/server.js
+++ src/server.js
@@ -34,12 +34,43 @@ app.use((req, res, next) => {
 });
 
 // 原版数据库连接
-const db = mysql.createConnection({
+var db = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: '123456',
   database: 'GreatMingWeb'
 });
+
+var db_config = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '123456',
+    database: 'GreatMingWeb'
+};
+
+function handleDisconnect() {
+    db = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+
+    db.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    db.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+
+handleDisconnect(); // 我不确定应该在这里调用这个函数还是在每个api里面都调用一遍。
 
 // // Docker镜像的数据库链接(使用本地数据库而不是数据库镜像)
 // const dbConfig = {
